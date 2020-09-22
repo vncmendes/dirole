a4efc84dbc52e32a8cf9c278384d797b57a4c0af
Vinicius Mendes <vinicius.mendes@josapar.com.br>
2020-09-22 10:57:38 -0300

    modificado a organizacao do código

--- view/index.php
+++ view/index.php
@@ -9,25 +9,315 @@
 
 <body>
 
-  <?php require_once 'navbar.php' ?>
+<!-- HEADER -->
+<header id="main-header">
+  <div class="content1">
+    <nav>
+      <ul>
+        <li>
+          <a href="eventos.php" class="nodecore navlinks">Eventos</a>
+        </li>
+        <li>
+          <a href="#" class="nodecore navlinks">Quem Somos</a>
+        </li>
+        <li>
+          <a href="#" class="nodecore navlinks">O que fazemos?</a>
+        </li>
+      </ul>
+    </nav>
 
-  <?php require_once 'ontherise.php' ?>
+    <!-- <img class="navicons" src="images/logo.svg" alt="" /> -->
+    <h1><a class="dirole" href="index.php">DiRolê</a></h1>
+    <div class="side">
+      <div class="dropdown">
+        <button class="btn dropdown-toggle dropbtnmod" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Criar Conta
+        </button>
+        <div class="dropdown-menu mydropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item mydrop-item" href="#" onclick="signUPB()">Baladeiro</a>
+          <a class="dropdown-item mydrop-item" href="#" onclick="signUPP()">Produtor</a>
+        </div>
+      </div>
+      <input type="text" placeholder="Procurar evento" />
+      <!-- <img class="avatarlogo" src="/images/avatar.png" alt=""/> -->
+      <a href="#" class="nodecore navlinks" id="btnlogin" onclick="modalLogin()">Entrar</a>
+    </div>
+  </div>
+</header>
+<!-- HEADER FIM -->
 
-  <?php require_once 'second-content.php' ?>
+<!-- COMEÇO DO SLIDER -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <?php require_once 'signUPB.php' ?>
+  <?php require_once 'signUPP.php' ?>
+  <?php require_once 'modalLogin.php' ?>
 
-  <?php require_once 'third-content.php' ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="../js/functions.js"></script>
 
-  <?php require_once 'rank-content.php' ?>
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../images/01.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../images/03.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../images/04.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- FIM SLIDER -->
+
+<!-- CARDS EVENTS -->
+<section class="eventcards">
+  <span class="indexontherise">Em Alta</span>
+  <div class="eventcardscontent">
+    <div class="cards">
+      <a href="#">
+        <ul>
+          <div class="cardimage">
+            <li><img src="../images/01.jpg" alt=""></li>
+          </div>
+
+          <div class="cardinfo">
+            <li>
+              <span class="eventname">Nome: </span>
+            </li>
+            <li>
+              <span class="eventdate">Data: </span>
+            </li>
+            <li>
+              <span class="eventlocation">Local: </span>
+            </li>
+          </div>
+        </ul>
+      </a>
+    </div>
+    <div class="cards">
+      <a href="#">
+        <ul>
+          <div class="cardimage">
+            <li><img src="../images/02.jpg" alt=""></li>
+          </div>
+
+          <div class="cardinfo">
+            <li>
+              <span class="eventname">Nome: </span>
+            </li>
+            <li>
+              <span class="eventdate">Data: </span>
+            </li>
+            <li>
+              <span class="eventlocation">Local: </span>
+            </li>
+          </div>
+        </ul>
+      </a>
+    </div>
+    <div class="cards">
+      <a href="#">
+        <ul>
+          <div class="cardimage">
+            <li><img src="../images/03.jpg" alt=""></li>
+          </div>
+
+          <div class="cardinfo">
+            <li>
+              <span class="eventname">Nome: </span>
+            </li>
+            <li>
+              <span class="eventdate">Data: </span>
+            </li>
+            <li>
+              <span class="eventlocation">Local: </span>
+            </li>
+          </div>
+        </ul>
+      </a>
+    </div>
+    <div class="cards">
+      <a href="#">
+        <ul>
+          <div class="cardimage">
+            <li><img src="../images/04.jpg" alt=""></li>
+          </div>
+
+          <div class="cardinfo">
+            <li>
+              <span class="eventname">Nome: </span>
+            </li>
+            <li>
+              <span class="eventdate">Data: </span>
+            </li>
+            <li>
+              <span class="eventlocation">Local: </span>
+            </li>
+          </div>
+        </ul>
+      </a>
+    </div>
+  </div>
+</section>
+<!-- CARDS EVENTS FIM -->
+
+<section class="second-content">
+  <div class="box-content1">
+    <div class="ourservices">
+      <span></span>
+    </div>
+    <div class="title1">
+      <span>Nosso objetivo é ajudar os promotores a divulgar e gerenciar seus eventos de forma gratuita; e deixar</span>
+    </div>
+    <div class="title2">
+      <span> os baladeiros por dentro dos principais acontecimentos que estão rolando na sua cidade.
+      </span>
+    </div>
+    <div class="title2">
+      <span>Com informações mais detalhadas e a possibilidade da venda dos ingressos pela plataforma conseguimos maximizar o valor gerado para ambos;
+      </span>
+    </div>
+  <div class="title2">
+    <span>e com isso se cria uma melhor experiência aos usuários e colaboradores envolvidos.
+    </span>
+  </div>
+</div>
+<div class="divsecond-content">
+  <div class="d3">
+    <ul>
+      <li><img src="../images/felicidades.svg" alt="icon"></li>
+      <li>
+        <span>Cervejeiro</span>
+      </li>
+    </ul>
+  </div>
+  <div class="d3">
+    <ul>
+      <li><img src="../images/fogos-de-artificio.svg" alt="icon"></li>
+      <li>
+        <span>Shows</span>
+      </li>
+    </ul>
+  </div>
+  <div class="d3">
+    <ul>
+      <li><img src="../images/garfo.svg" alt="icon"></li>
+      <li>
+        <span>Gastronômico</span>
+      </li>
+    </ul>
+  </div>
+  <div class="d3">
+    <ul>
+      <li><img src="../images/danca.svg" alt="dance"></li>
+      <li>
+        <span>Pubs e Baladas</span>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="d3">
+  <ul>
+    <li><img class="clientpresent" src="../images/presente.svg" alt="present"></li>
+    <li>
+      <span>Bônus Clientes Ativos</span>
+    </li>
+  </ul>
+</div>
+</section>
+
+<!-- CONTATO -->
+<section class="third-content">
+  <div class="divcontact-content">
+    <div class="d4">
+      <h2>FALE CONOSCO</h2>
+    </div>
+    <div class="contact-form">
+      <form class="form1" action="#" name="form">
+        <fieldset>
+          <label for="name"></label>
+          <input type="text" name="name" pattern="" placeholder="DIGITE SEU NOME" required>
+          <label for="email"></label>
+          <input type="text" name="email" placeholder="DIGITE SEU E-MAIL" pattern="" required>
+          <label for="name"></label>
+          <textarea name="mesage" id="mesage" placeholder="ESCREVE UMA MENSAGEM" required></textarea>
+          <label for="button"></label>
+          <button name="btn" type="submit">Enviar</button>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</section>
+<!-- CONTATO FIM -->
+
+<!-- RANK CONTENT -->
+  <section class="rank-content">
+    <div class="ourservicesrank">
+      <span>SENHORES DA BALADA</span>
+    </div>
+    <div class="divrank-content">
+      <div class="d3rank">
+        <ul>
+          <li><img src="../images/avatar.png" alt="icon"></li>
+          <li>
+            <span>Garça</span>
+          </li>
+        </ul>
+      </div>
+      <div class="d3rank">
+        <ul>
+          <li><img src="../images/avatar.png" alt="icon"></li>
+          <li>
+            <span>Manoel</span>
+          </li>
+        </ul>
+      </div>
+      <div class="d3rank">
+        <ul>
+          <li><img src="../images/avatar.png" alt="icon"></li>
+          <li>
+            <span>João</span>
+          </li>
+        </ul>
+      </div>
+      <div class="d3rank">
+        <ul>
+          <li><img src="../images/avatar.png" alt="icon"></li>
+          <li>
+            <span>Deadpool</span>
+          </li>
+        </ul>
+      </div>
+      <div class="d3rank">
+        <ul>
+          <li><img src="../images/avatar.png" alt="icon"></li>
+          <li>
+            <span>Usuário1</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+</section>
+<!-- RANK CONTENT FIM -->
 
   <?php require_once  '../controler/usuario.php' ?>
   <?php require_once  '../controler/provider.php' ?>
 
   <?php require_once 'footer.php' ?>
 
-
-  <!-- Aways last scripts -->
-
-  <!-- End scripts -->
 </body>
 
 </html>
--- view/perfil.php
+++ view/perfil.php
@@ -15,58 +15,62 @@
 	?>
 
 </head>
-<div class="principal py-4">
-	<div style="height: 57.8%;" class="container mr-5 py-5">
-		<div class="row">
-			<div class="col-4 col-md-3 border bg-light">
-				<div class="text-center">
-					<h1><?php echo $usuario->nome ?></h1>
-					<form action="../controler/usuario.php" method="POST" enctype="multipart/form-data">
-						<hr><img style="width: 100px; height: 100px;" src="../imagens/<?= $usuario->foto ?>" class="img-thumbnail"></hr>
-						<hr><label for="arquivo">Selecione a imagem</label>
-						<input type="file" class="form-control-file ml-3" name="foto"></hr>
-						<hr><button type="submit" class="btn btn-success" name="uploadArc">Enviar</button></hr>
-					</form>
+<body>
+	<?php
+  		require_once 'navbar2.php'
+  	?>
+	<div class="principal py-4">
+		<div style="height: 57.8%;" class="container mr-5 py-5">
+			<div class="row">
+				<div class="col-4 col-md-3 border bg-light">
+					<div class="text-center">
+						<h1><?php echo $usuario->nome ?></h1>
+						<form action="../controler/usuario.php" method="POST" enctype="multipart/form-data">
+							<hr><img style="width: 100px; height: 100px;" src="../imagens/<?= $usuario->foto ?>" class="img-thumbnail"></hr>
+							<hr><label for="arquivo">Selecione a imagem</label>
+							<input type="file" class="form-control-file ml-3" name="foto"></hr>
+							<hr><button type="submit" class="btn btn-success" name="uploadArc">Enviar</button></hr>
+						</form>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-4 border bg-light">
-				<p>Nome:</p>
-				<hr>
-				<p>Sobrenome:</p>
-				</hr>
-				<hr>
-				<p>Email:</p>
-				</hr>
-				<hr>
-				<p>Senha:</p>
-				</hr>
-				<div class="row">
-					<form action="editar-perfil.php" method="post">
-						<input type="hidden" name="id" value="<?= $usuario->id ?>">
-						<button type="submit" class="btn btn-secondary mr-3 ml-3" name="editarPerfil">Alterar</button>
-					</form>
-					<form action="../controler/usuario.php" method="post">
-						<input type="hidden" name="id" value="<?= $usuario->id ?>">
-						<button class="btn btn-danger" type="submit" name="removerPerfil">Remover</button>
-					</form>
+				<div class="col-4 border bg-light">
+					<p>Nome:</p>
+					<hr>
+					<p>Sobrenome:</p>
+					</hr>
+					<hr>
+					<p>Email:</p>
+					</hr>
+					<hr>
+					<p>Senha:</p>
+					</hr>
+					<div class="row">
+						<form action="editar-perfil.php" method="post">
+							<input type="hidden" name="id" value="<?= $usuario->id ?>">
+							<button type="submit" class="btn btn-secondary mr-3 ml-3" name="editarPerfil">Alterar</button>
+						</form>
+						<form action="../controler/usuario.php" method="post">
+							<input type="hidden" name="id" value="<?= $usuario->id ?>">
+							<button class="btn btn-danger" type="submit" name="removerPerfil">Remover</button>
+						</form>
+					</div>
 				</div>
-			</div>
-			<div class="col-4 border bg-light">
-				<p><?php echo "$usuario->nome"; ?></p>
-				<hr>
-				<p><?php echo "$usuario->sobrenome"; ?></p>
-				</hr>
-				<hr>
-				<p><?php echo "$usuario->email"; ?></p>
-				</hr>
-				<hr>
-				<p><?php echo "$usuario->senha"; ?></p>
-				</hr>
+				<div class="col-4 border bg-light">
+					<p><?php echo "$usuario->nome"; ?></p>
+					<hr>
+					<p><?php echo "$usuario->sobrenome"; ?></p>
+					</hr>
+					<hr>
+					<p><?php echo "$usuario->email"; ?></p>
+					</hr>
+					<hr>
+					<p><?php echo "$usuario->senha"; ?></p>
+					</hr>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-
+</body>
 </html>
 <?php include "footer.php"; ?>
